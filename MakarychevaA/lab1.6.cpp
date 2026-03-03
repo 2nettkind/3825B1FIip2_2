f09b3cc0a448b04771c3111c6a355cbfdf6fda97
NastyaMakarycheva <nastyaa_mak@mail.ru>
2026-03-03 11:25:51 +0300

    changes

--- MakarychevaA/lab1.6.cpp
+++ MakarychevaA/lab1.6.cpp
@@ -7,19 +7,19 @@ public:
 	Temperature(float temp) {
 		temperature = temp;
 	}
-	float GetTemperature() {
+	float GetTemperature() const {
 		return temperature;
 	}
 	void SetTemperature(float temp) {
 		temperature = temp;
 	}
-	float ConversionFahrenheit() {
+	float ConversionFahrenheit() const {
 		return temperature * 1.8f + 32;
 	}
-	float ConversionKelvin() {
+	float ConversionKelvin() const {
 		return temperature + 273.15f;
 	}
-	friend std::ostream& operator<<(std::ostream& os, Temperature& t) {
+	friend std::ostream& operator<<(std::ostream& os, const Temperature& t) {
 		os << "Temperature:" << t.temperature << "C,"<< t.ConversionFahrenheit() << "F,"<< t.ConversionKelvin() << "K";
 		return os;
 	}
@@ -30,6 +30,10 @@ int main() {
 	float temp = 0;
 	std::cout << "Enter the degrees (in Celsius)\n";
 	std::cin >> temp;
+	if (temp < -273.15f) {
+		std::cout << "Error.The temperature cannot be below absolute zero (-273.15C)\n";
+		return 1;
+	}
 	Temperature a(temp);
 	std::cout << "Enter mode\n" "1.Convert to Fahrenheit\n" "2.Convert to Kelvin\n" "3.Console output\n";
 	std::cin >> choice;
